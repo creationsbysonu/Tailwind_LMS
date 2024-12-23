2b44d889db350220bd1913d06bcfa6d5e7545ed1
sonuwa <sonugupta.ioepc.edu.np@gmail.com>
2024-12-23 20:30:06 +0545

    first commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
-import Card from '../components/card'
+import Card from '../components/Card'
+import axios from 'axios'
 
+//connecting backend 
 const Home = () => {
+  const [books,setBooks] = useState([])
+  const fetchbooks = async()=>{
+    const response = await axios.get('http://localhost:3000/book')
+    // console.log(response.status)  
+    // console.log(response.data.data)   //response.data for recieving data and .data for target data and this .data is mentioned in backend in all read section
+
+    if (response.status==200){
+      setBooks(response.data.data)
+    }
+
+  }
+
+  useEffect(()=>{
+    fetchbooks()
+
+  },[])
+
+  console.log(books)
+
   return (
     <>
          <Navbar /> 
          <div className='flex flex-wrap justify-normal'>
-         <Card />
-         <Card />
-         <Card />
-         <Card />
+            
+            {
+              books.length > 0 && books.map((book)=>{
+              return (
+                <Card book={book}/>
+              )
+              })
+            }
          </div>
         
     </>
