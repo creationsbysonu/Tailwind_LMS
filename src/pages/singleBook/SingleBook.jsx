8411487f0c3802500f93b2b9c77af61e1747118a
sonuwa <sonugupta.ioepc.edu.np@gmail.com>
2025-01-01 17:52:01 +0545

    first test

--- src/pages/singleBook/SingleBook.jsx
+++ src/pages/singleBook/SingleBook.jsx
@@ -43,7 +43,7 @@ const SingleBook = () => {
           <div class='flex flex-wrap justify-normal'>
             <div class="max-w-sm bg-violet-100 border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-2 mb-2 mt-2 mr-8 hover:shadow-black transition duration-500 ">
             <a href="#">
-                <img class="rounded-t-lg w-50 h-50" src={"https://static.vecteezy.com/system/resources/previews/006/115/725/non_2x/black-and-white-open-book-logo-illustration-on-white-background-free-vector.jpg"} alt="" />
+                <img class="rounded-t-lg w-50 h-50" src={book.image} alt="" />
             </a>
               <div class="p-5">
                   <a href="#">
@@ -52,6 +52,8 @@ const SingleBook = () => {
                   <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{book.bookPrice}</p>
                   <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{book.isbnNumber}</p>
                   <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{book.authorName}</p>
+                  <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{book.publication}</p>
+                  <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{book.publishedAt}</p>
                   <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Read more</button>    
                
               <button type="button" class="text-white bg-green-400 hover:bg-green-600 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"><Link to={`/editBook/${book._id}`}>Edit Book</Link></button> 
